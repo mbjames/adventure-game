fc32c8a94b737861b1b84132805418998f9db386
Michael J Bailey <mbjames203@gmail.com>
2021-03-23 16:10:41 -0700

    initial commit

---
+++ room_questions.py
@@ -0,0 +1,87 @@
+def start_questions():
+    print("\n You awake in a dark room.")
+    print(" There are 4 paths leading out.")
+    print(" Which path will you take? (1, 2, 3 or 4)")
+    print(" 1. Take the 1st path?")
+    print(" 2. Take the 2nd path?")
+    print(" 3. Take the 3rd path?")
+    print(" 4. Take the 4th path?")
+
+
+def bear_questions():
+    print("\n You have entered a room with a bear.")
+    print(" The bear is blocking a door.")
+    print(" What will you do? (1 or 2)")
+    print(" 1. Take the honey?")
+    print(" 2. Taunt the bear?")
+
+
+def monster_questions():
+    print("\n You have entered a room with a monster.")
+    print(" The monster is blocking a door.")
+    print(" What will you do? (1 or 2)")
+    print(" 1. Throw a rock?")
+    print(" 2. Taunt the monster?")
+
+
+def diamond_questions():
+    print("\n You have entered a room full of diamonds.")
+    print(" There is a door in the back of the room.")
+    print(" What will you do? (1 or 2)")
+    print(" 1. Fill your pockets full of diamonds?")
+    print(" 2. Forget the diamonds and head for the door?")
+
+
+def vine_questions():
+    print("\n You have entered a room full of vines.")
+    print(" The vines appear to be moving and are blocking a door.")
+    print(" What will you do? (1 or 2)")
+    print(" 1. Cut the vines?")
+    print(" 2. Set the vines on fire?")
+
+
+def dungeon_questions():
+    print("\n You have entered a dungeon room.")
+    print(" There are no doors in the back of the room.")
+    print(" There is a demon looking to make a deal.")
+    print(" What will you do? (1 or 2)")
+    print(" 1. Sell your soul and escape?")
+    print(" 2. Give the demon your gold to see the hidden passage?")
+
+
+def goblin_questions():
+    print("\n You have entered a room full of goblins.")
+    print(" The goblins seem harmless.")
+    print(" There are 4 paths leading out.")
+    print(" Which path will you take? (1, 2, 3 or 4)")
+    print(" 1. Take the 1st path?")
+    print(" 2. Take the 2nd path?")
+    print(" 3. Take the 3rd path?")
+    print(" 4. Take the 4th path?")
+
+
+def ruby_questions():
+    print("\n You have entered a room full of rubies.")
+    print(" There is a door in the back of the room.")
+    print(" What will you do? (1 or 2)")
+    print(" 1. Fill your pockets full of rubies?")
+    print(" 2. Forget the rubies and head for the door?")
+
+
+def sand_questions():
+    print("\n You have entered a room full of sand.")
+    print(" There is a door in the back of the room.")
+    print(" What will you do? (1 or 2)")
+    print(" 1. Head for the door?")
+    print(" 2. Go back the way you came?")
+
+
+def final_room():
+    print("\n You have entered the final room.")
+    print(" There are 5 paths leading out.")
+    print(" Which path will you take? (1, 2, 3, 4 or 5)")
+    print(" 1. Take the 1st path?")
+    print(" 2. Take the 2nd path?")
+    print(" 3. Take the 3rd path?")
+    print(" 4. Take the 4th path?")
+    print(" 5. Take the 5th path?")
