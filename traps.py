0f11002ed5da81b001a267a2a84926cab17844e5
Michael J Bailey <mbjames203@gmail.com>
2021-03-23 15:45:27 -0700

    Delete traps.py

--- traps.py
+++
@@ -1,8 +0,0 @@
-trap_door = "\n You stepped on a trap door and fell to your death."
-
-trap_spikes = "\n Wooden spikes shot up from the floor and you died."
-
-trap_fire = "\n A fireball engulfed you and you died."
-
-trap_arrows = "\n Arrows shot out of the wall and you died."
-
